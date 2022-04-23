3e27a9681399ccd6764339f3451aedd056cfbb40
m7md.ab911@gmail.com <moeabdeen@Mohammad-MacBook.local>
2022-04-23 23:11:24 +0300

    uiNew

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css'],
 })
 export class CustomersComponent implements OnInit {
+  faSearch = faSearch;
   constructor() {}
 
   ngOnInit(): void {}
