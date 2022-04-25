36378943624c219532c0da4990f7137ef63fe492
m7md.ab911@gmail.com <moeabdeen@Mohammad-MacBook.local>
2022-04-25 22:57:37 +0300

    crud

--- src/app/components/dash-board/dash-board.component.ts
+++ src/app/components/dash-board/dash-board.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from 'src/app/interfaces/contact';
+import { Customer } from 'src/app/interfaces/customer';
+import { ContactsService } from 'src/app/services/contacts.service';
+import { CustomersService } from 'src/app/services/customers.service';
 
 @Component({
   selector: 'app-dash-board',
@@ -6,7 +10,21 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./dash-board.component.css'],
 })
 export class DashBoardComponent implements OnInit {
-  constructor() {}
+  contacts: any[] = [];
+  customers: any[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private contactSer: ContactsService,
+    private customerSer: CustomersService
+  ) {}
+
+  ngOnInit(): void {
+    this.customerSer.getCustomer().subscribe((customers: Customer[]) => {
+      this.customers = customers.splice(0, 5);
+    });
+
+    this.contactSer.getContact().subscribe((contacts: Contact[]) => {
+      this.contacts = contacts.splice(0, 5);
+    });
+  }
 }
